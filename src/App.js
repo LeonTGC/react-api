84f81a98fe5e6127da715f5ae047df800729abce
LeonTGC <leon.cooper@gceducationandskills.ac.uk>
2024-02-28 12:10:30 +0000

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react"
 
-function App() {
+const App = () => {
+
+  const [data, setData] = useState("")
+  const [error, setError] = useState(null)
+
+  const fetchData = async () => {
+    try {
+      let response = await fetch('https://api.adviceslip.com/advice')
+      if(!response.ok) {
+        throw new Error(response.statusText)
+      }
+      let advice = await response.json()
+      setData(advice)
+    } catch(error) {
+      setError('could not fetch data')
+      console.log(error.message)
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  if(!data){
+    return <h1>loading...</h1>
+  }
+  if(error){
+    return <h1>{error}</h1>
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>some text</h1>
+      <h2>{data.slip.advice}</h2>
+      <button onClick={fetchData}>fetch</button>
     </div>
-  );
+  )
 }
-
-export default App;
+export default App
